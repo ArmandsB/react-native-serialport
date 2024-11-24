5b0115f9d991b3c103495ebdcbebbd1381f3e499
Armands Baurovskis <baurovskis.armands@gmail.com>
2024-11-24 14:06:06 +0200

    Update to fix iOS

--- index.js
+++ index.js
@@ -52,20 +52,22 @@ const actions = {
   ON_READ_DATA            : 'onReadDataFromPort'
 };
 
-RNSerialport.intArrayToUtf16 = (intArray) => {
-  var str = "";
-  for (var i = 0; i < intArray.length; i++) {
-    str += String.fromCharCode(intArray[i]);
+if (RNSerialport) {
+  RNSerialport.intArrayToUtf16 = (intArray) => {
+    var str = "";
+    for (var i = 0; i < intArray.length; i++) {
+      str += String.fromCharCode(intArray[i]);
+    }
+    return str;
   }
-  return str;
-}
-RNSerialport.hexToUtf16 = (hex) => {
-  var str = "";
-  var radix = 16;
-  for (var i = 0; i < hex.length && hex.substr(i, 2) !== "00"; i += 2) {
-    str += String.fromCharCode(parseInt(hex.substr(i, 2), radix));
+  RNSerialport.hexToUtf16 = (hex) => {
+    var str = "";
+    var radix = 16;
+    for (var i = 0; i < hex.length && hex.substr(i, 2) !== "00"; i += 2) {
+      str += String.fromCharCode(parseInt(hex.substr(i, 2), radix));
+    }
+    return str;
   }
-  return str;
 }
 
 module.exports = { RNSerialport, definitions, actions };
